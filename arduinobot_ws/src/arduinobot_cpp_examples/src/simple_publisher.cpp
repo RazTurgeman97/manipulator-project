d1ab251d17b5fe5bfb1cb86fec797b21ba28fa13
Raz Turgeman <Raz.Turgeman97@gmail.com>
2024-11-27 23:43:58 +0200

    minor

--- arduinobot_ws/src/arduinobot_cpp_examples/src/simple_publisher.cpp
+++ arduinobot_ws/src/arduinobot_cpp_examples/src/simple_publisher.cpp
@@ -1,44 +1,44 @@
-#include <rclcpp/rclcpp.hpp>
-#include <std_msgs/msg/string.hpp>
+#include <rclcpp/rclcpp.hpp> // ROS2 C++ API
+#include <std_msgs/msg/string.hpp> // ROS2 std_msgs
 
-#include <chrono>
+#include <chrono> // C++ chrono library
 
-using namespace std::chrono_literals;
+using namespace std::chrono_literals; // Using namespace for the literals
 
 
-class SimplePublisher : public rclcpp::Node{
+class SimplePublisher : public rclcpp::Node{ // SimplePublisher class inherits from rclcpp::Node
 
-public:
+public: // Public members
 
-    SimplePublisher() : Node("simple_publisher"), counter_(0){
+    SimplePublisher() : Node("simple_publisher"), counter_(0){ // Constructor for SimplePublisher class
 
-        pub_ = create_publisher<std_msgs::msg::String>("chatter", 10);
-        timer_ = create_wall_timer(1s, std::bind(&SimplePublisher::timerCallback, this));
+        pub_ = create_publisher<std_msgs::msg::String>("chatter", 10); // Create a publisher to the topic "chatter"
+        timer_ = create_wall_timer(1s, std::bind(&SimplePublisher::timerCallback, this)); // Create a timer to publish messages at 1 Hz
 
-        RCLCPP_INFO(get_logger(), "Publishing at 1 Hz");
+        RCLCPP_INFO(get_logger(), "Publishing at 1 Hz"); // Print a message to the console
     }
 
     void timerCallback(){
-        auto message = std_msgs::msg::String();
-        message.data = "Hello ROS2 - counter: " + std::to_string(counter_++);
-        pub_->publish(message);
+        auto message = std_msgs::msg::String(); // Create a message
+        message.data = "Hello ROS2 - counter: " + std::to_string(counter_++); // Set the message data
+        pub_->publish(message); // Publish the message
     }
 
 private:
 
-    unsigned int counter_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_;
-    rclcpp::TimerBase::SharedPtr timer_;
+    unsigned int counter_; // Counter for the number of messages published
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_; // Publisher to the topic "chatter"
+    rclcpp::TimerBase::SharedPtr timer_; // Timer to publish messages at 1 Hz
 
 };
 
 
 int main(int argc, char* argv[]){
 
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<SimplePublisher>();
-    rclcpp::spin(node);
-    rclcpp::shutdown();
+    rclcpp::init(argc, argv); // Initialize ROS2
+    auto node = std::make_shared<SimplePublisher>(); // Create a shared pointer to a SimplePublisher object
+    rclcpp::spin(node); // Spin the node
+    rclcpp::shutdown(); // Shutdown ROS2
     return 0;
     
 }
--- arduinobot_ws/src/arduinobot_cpp_examples/src/simple_subscriber.cpp
+++ arduinobot_ws/src/arduinobot_cpp_examples/src/simple_subscriber.cpp
@@ -1,32 +1,32 @@
-#include <rclcpp/rclcpp.hpp>
-#include <std_msgs/msg/string.hpp>
+#include <rclcpp/rclcpp.hpp> // ROS2 C++ API
+#include <std_msgs/msg/string.hpp> // ROS2 std_msgs
 
 using std::placeholders::_1;
 
-class SimpleSubscriber : public rclcpp::Node{
+class SimpleSubscriber : public rclcpp::Node{ // SimpleSubscriber class inherits from rclcpp::Node
 
 public:
 
-    SimpleSubscriber() : Node("simple_subscriber"){
+    SimpleSubscriber() : Node("simple_subscriber"){ // Constructor for SimpleSubscriber class
 
-        sub_ = create_subscription<std_msgs::msg::String>("chatter", 10, std::bind(&SimpleSubscriber::msgCallback, this, _1));
+        sub_ = create_subscription<std_msgs::msg::String>("chatter", 10, std::bind(&SimpleSubscriber::msgCallback, this, _1)); // Create a subscription to the topic "chatter"
     }
 
-    void msgCallback(const std_msgs::msg::String &msg) const{
+    void msgCallback(const std_msgs::msg::String &msg) const{ // Callback function for the subscription
 
-        RCLCPP_INFO_STREAM(get_logger(), "I heared: " << msg.data.c_str());
+        RCLCPP_INFO_STREAM(get_logger(), "I heared: " << msg.data.c_str()); // Print the message to the console
     }
 
 private:
 
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_; 
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_; // Subscription to the topic
 };
 
 int main(int argc, char * argv[]){
 
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<SimpleSubscriber>();
-    rclcpp::spin(node);
-    rclcpp::shutdown();
+    rclcpp::init(argc, argv); // Initialize ROS2    
+    auto node = std::make_shared<SimpleSubscriber>(); // Create a shared pointer to a SimpleSubscriber object
+    rclcpp::spin(node); // Spin the node
+    rclcpp::shutdown(); // Shutdown ROS2
     return 0;
 }
